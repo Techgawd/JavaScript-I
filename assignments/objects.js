9bd6b42865f40386888e67e65befb9a3f15ffd25
Techgawd <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2019-04-17 20:30:49 -0400

    finished objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -28,8 +28,11 @@ const kennan = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
-}
+  "gender": "M",
+  speak: function(){
+    return "My name is " + kennan.name + "!"},
+  }
+
 
 const keven = {
   "id": 3,
@@ -49,7 +52,10 @@ const antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
+  "gender": "F",
+  multiplyNums: function (a, b){
+   return a * b
+  }
 }
 
 // Write your intern objects here:
@@ -72,8 +78,12 @@ console.log(antonietta.gender);
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(kennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
